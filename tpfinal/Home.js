9889285595fd2c1c5b8c2c60e683c0b17f8eecde
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-16 09:46:19 -0300

    flujo

--- tpfinal/Home.js
+++ tpfinal/Home.js
@@ -5,7 +5,56 @@ import { ActionTypes, useContextState, setContextState, contextState } from './n
 
 export default function Home({ navigation }) {
 
-    return {
-        
-    }
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.centrar}>
+                <TouchableOpacity style={styles.boton} onPress={() => navigation.navigate("Camara")}>
+                    <Text style={styles.texto}>Abrir camara</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.boton} onPress={() => navigation.navigate("Files")}>
+                    <Text style={styles.texto}>Seleccionar foto o video</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.boton} onPress={() => navigation.navigate("Notes")}>
+                    <Text style={styles.texto}>Block de Notas</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.boton} onPress={() => navigation.navigate("MisSaves")}>
+                    <Text style={styles.texto}>Mis Archivos</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    boton: {
+        borderRadius: 15,
+        borderColor: '#58CBC0',
+        backgroundColor: '#58CBC0',
+        minHeight: 50,
+        minWidth: 200,
+        margin: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    texto: {
+        display: 'flex',
+        color: 'white',
+        display: 'flex',
+        alignContent: 'center',
+        justifyContent: 'center',
+        fontSize: 18,
+    },
+    centrar: {
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
